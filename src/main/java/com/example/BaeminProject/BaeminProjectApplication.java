7d67c89b54cb64123c5a77b29114ec5a5bf79125
seohee99 <imshee99@gmail.com>
2024-05-03 15:51:57 +0900

    Init: initial project

---
+++ src/main/java/com/example/BaeminProject/BaeminProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.BaeminProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeminProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeminProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BaeminProject/BaeminProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BaeminProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeminProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
